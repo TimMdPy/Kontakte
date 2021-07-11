a3172c2b016aab9f324d50d4528dc00818b733f4
Tim Meyerdiercks <timmd.development.contact@gmail.com>
2021-07-11 10:57:14 +0200

    Create the AddContactViewController Subclass for the addContact UI.

---
+++ Contact/AddContactViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AddContactViewController.swift
+//  Contact
+//
+//  Created by Tim Meyerdiercks on 11.07.21.
+//
+
+import UIKit
+
+class AddContactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
