d67d2347f9256ae2a5202c213313a3bb26de7910
Tim Meyerdiercks <timmd.development.contact@gmail.com>
2021-07-11 11:00:50 +0200

    Create Actions and Outlets in the AddContactViewController.

--- Contact/AddContactViewController.swift
+++ Contact/AddContactViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class AddContactViewController: UIViewController {
-
+    
+    // MARK: Outlets
+    @IBOutlet weak var finishButton: UIButton!
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
     }
+    
+    
+    // MARK: - Actions
+    @IBAction func cancelButton_Tapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func finishButton_Tapped(_ sender: UIButton) {
+    }
+    
     
 
 
