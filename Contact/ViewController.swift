da7f9e169bf3291dfc419c48495ff2ea995dbc61
Tim Meyerdiercks <timmd.development.contact@gmail.com>
2021-07-11 10:12:43 +0200

    Create Global Variablen: contacts array and the persistent container context. Create and fill the two extensions for tableview and create a test Contact item.

--- Contact/ViewController.swift
+++ Contact/ViewController.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 
+// MARK: - Global Variablen
+var contacts: [Contact] = []
+let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+
 class ViewController: UIViewController {
     
     // MARK: - Outlets
@@ -18,6 +23,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Create default test contact
+        let defaultContact = Contact(context: context)
+        defaultContact.firstName = "Max"
+        defaultContact.lastName = "Mustermann"
+        defaultContact.phoneNumber = "+49 123 45678910"
+        
+        contacts.append(defaultContact)
     }
     
     
@@ -27,3 +40,38 @@ class ViewController: UIViewController {
     
 }
 
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "contactCell", for: indexPath)
+        
+        // Setup cell style
+        let contact = contacts[indexPath.row]
+        
+        if contact.firstName != nil && contact.lastName != nil {
+            cell.textLabel?.text = contact.firstName! + " " + contact.lastName!
+        }else if contact.firstName != nil {
+            cell.textLabel?.text = contact.firstName!
+        }else if contact.lastName != nil {
+            cell.textLabel?.text = contact.lastName!
+        }
+        
+        if contact.phoneNumber != nil {
+            cell.detailTextLabel?.text = contact.phoneNumber!
+        }
+        
+        
+        return cell
+    }
+    
+    
+}
+
+extension ViewController: UITableViewDelegate {
+    
+}
